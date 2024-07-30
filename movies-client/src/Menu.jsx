0a0cb741b6cd387afe6477dbf3e8150a71dfdabb
srreed3 <rosereed1212@gmail.com>
2024-07-30 14:42:06 -0400

    completed header and footer with styling

--- movies-client/src/Menu.jsx
+++ movies-client/src/Menu.jsx
@@ -1,16 +1,17 @@
 import { NavLink } from "react-router-dom"
+import "./menu.css"
 
 function Menu() {
     return (
         <>
             <header>
                 <div className="header-container">
-                <h1><img src="images/play.png" alt="Play button logo" id="play_logo"/> M.O.M.</h1>
+                <h1><img src="public/images/play.png" alt="Play button logo" id="play_logo"/> M.O.M.</h1>
                 <p>Movie Organizational Magic</p>
                 </div>
 
                 <div className="navbar">
-                <p><NavLink to ="/">HOME</NavLink></p>
+                <p><NavLink to ="/" id="home-container">HOME</NavLink></p>
                 <div className="dropdown">
                     <button className="dropbtn">MOVIES<i className="fa fa-caret-down"></i></button>
                     <div className="dropdown-content">
